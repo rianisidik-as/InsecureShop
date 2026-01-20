af527e6312ffbe0c90afb3b28584941b09d588f4
Riani <riani@aikido.dev>
2026-01-20 13:58:51 +0800

    Empty commit to trigger PR update

--- InsecureShop-android/app/src/main/index.js
+++ InsecureShop-android/app/src/main/index.js
@@ -1,7 +1,7 @@
 const app = {}
 
 app.get("/user", (req, res) => {
-    const connStr = "Server=tcp:myserver.database.windows.net,1433;Initial Catalog=mydb;Persist Security Info=False;User ID=myuser;Password=12345password";
+    const connStr = "Server=tcp:myserver.database.windows.net,1433;Initial Catalog=mydb;Persist Security Info=False;User ID=myuser;Password=nopassword";
     const username = req.query.username
     const unsafeQuery = `SELECT * FROM users WHERE username = '${username}'`
     sql.connect(connStr).query(unsafeQuery, (err, result) => {
