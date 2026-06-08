18a2d2732f2c65906f25eb86ce73f5510bc1f57c
rianisidik-as <riani@aikido.dev>
2026-06-08 16:57:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = {}
 app.get("/user", (req, res) => {
     const connStr = "Server=tcp:myserver.database.windows.net,1433;Initial Catalog=mydb;Persist Security Info=False;User ID=myuser;Password=$uperSecret123!@#";
     const username = req.query.username
-    const unsafeQuery = `SELECT * FROM users WHERE username = '${username}'` //no sec
+    const unsafeQuery = `SELECT * FROM users WHERE username = '${username}'` //nosec
     sql.connect(connStr).query(unsafeQuery, (err, result) => {
         res.status(200).send(result)
     })
